1e1cb5ee5faf0efa17889272b064a7b1d6d01bcd
Adam Carling <adamjcarling@gmail.com>
2022-10-26 14:44:31 -0600

    Intial commit

---
+++ MyWorld.java
@@ -0,0 +1,28 @@
+public class MyWorld {
+    
+    public static void main(String[] args) {
+        
+    }//End Main
+
+
+}//End MyWorld
+
+public interface Creature {
+
+}//End Creature
+
+public class Animal implements Creature {
+
+}// End Animal
+
+public class Cat extends Animal {
+
+}// End Cat
+
+public class Bunny extends Animal {
+
+}// End Bunny
+
+public class Fish extends Animal {
+
+}// End Fish
