355226e11c00cbae7fb9dff4ca671d9b76c7c5b9
Adam Carling <adamjcarling@gmail.com>
2022-10-27 16:41:19 -0600

    Finished MyWorld Class

--- MyWorld.java
+++ MyWorld.java
@@ -1,23 +1,52 @@
+import java.util.Random;
+
 /*
- * Editors: Adam Carling & 
+ * Editors: Adam Carling & Haley Bush
  * 
  * 
  */
 
-/*
- * I figure we split it up based on MyWorld and Creature/Animal. Which one do you want? 
- */
 
 public class MyWorld {
+    static float spawnChance = 0.5f;
+//Blades of grass is unused atm but will be used if we add in a death chance function later.
+    int numOfGrassBlades = 200;
+
     public static void main(String[] args) {
-        
-        run();
+        run(8);
 
     }//End Main
 //The run function should be kept seperate to allow for possible recursive calls and to keep the main() function looking clean.
-    public static void run() {
-
+    public static void run(int howlong) {
+    //The ticking can be thought of as each year the world exists. 
+    //future versions of code could be adjusted to allow the 
+    //user to suggest years while months are used in the program.
+        int years = howlong;
+        if(years < 1){
+            return;
+        }
+        years--;
+        
+        run(years);
+        System.out.println("The year is: " + (years+1) + "AC (After Creation). Such is the history.");
+        rollSpawnChance(spawnChance);
     }// End of Run function
+
+    public static void rollSpawnChance(float spawnChance) {
+        float f = (float)Math.random();
+        if(f < spawnChance+.01){
+            Animal.spawnAnimal();
+        }else{
+            gameText();
+        }
+    }
+
+//For possible alternatives for later usage.
+//Unimportant for now
+    public static String gameText(){
+        String s =" ";
+        return s;
+    }//End gameText add on
 }//End MyWorld
 
 
@@ -27,17 +56,29 @@ interface Creature {
 
 }//End Creature
 class Animal implements Creature {
-
+    public static void spawnAnimal(){
+        System.out.println("Creature Spawned");
+    }
 }// End Animal
 
 
 
 class Cat extends Animal {
 
+    public static void hunt(){
+        System.out.println("The cat is hunting...");
+    }
 }// End Cat
 class Bunny extends Animal {
-
+    public static void multiply(){
+        System.out.println("The bunny is multiplying...");
+    }
+    public static void eat(){
+        System.out.println("The bunny is eating...");
+    }
 }// End Bunny
-public class Fish extends Animal {
-
+ class Fish extends Animal {
+    public static void swim(){
+        System.out.println("The fish is swimming...");
+    }
 }// End Fish
